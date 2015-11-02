917d5e1e54029d25536e6f3e04f4f07efcf5ec08
Ernest Wong <ewon521@gmail.com>
2015-11-02 22:18:51 +1300

    Added initial opcontrol

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
+#include "drive.h"
+#include "flywheel.h"
+
+
+
 void operatorControl()
 {
-    // Note: never exit
+    flywheelRun(fwAbove);
+    flywheelRun(fwBelow);
 	while (1)
     {
+        driveUpdate(drive);
 		delay(20);
 	}
+    // Note: never exit
 }
