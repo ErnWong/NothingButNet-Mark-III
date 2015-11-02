2d56a433d1634669ba148d44677cc9e48437835c
Ernest Wong <ewon521@gmail.com>
2015-11-02 22:12:37 +1300

    Added initial global robot objects

--- include/main.h
+++ include/main.h
@@ -4,6 +4,7 @@
 #include <API.h>
 
 #include "utils.h"
+#include "drive.h"
 #include "flywheel.h"
 
 #ifdef __cplusplus
@@ -18,6 +19,14 @@ void autonomous();
 void initializeIO();
 void initialize();
 void operatorControl();
+
+
+// Robot:
+
+extern Pigeon * pigeon;
+extern Drive * drive;
+extern Flywheel * fwAbove;
+extern Flywheel * fwBelow;
 
 
 
