957584026afaac256cb3f72c043c64658bdcec5f
Ernest Wong <ewon521@gmail.com>
2015-11-10 21:59:13 +1300

    Fix: typo, raw->input

--- dashboard/server.js
+++ dashboard/server.js
@@ -73,19 +73,19 @@ function decode(input) {
     var sigMid = -1;
     var sigEnd = -1;
 
-    if (sigStart < 0) return createInfo(timestamp, channel, message, raw);
+    if (sigStart < 0) return createInfo(timestamp, channel, message, input);
 
     sigMid = input.indexOf('|', sigStart);
     sigEnd = input.indexOf(']', sigStart);
 
-    if (sigEnd < 0) return createInfo(timestamp, channel, message, raw);
+    if (sigEnd < 0) return createInfo(timestamp, channel, message, input);
     if (sigMid > sigEnd) sigMid = sigStart;
 
     timestamp = input.substring(sigStart + 1, sigMid).trim();
     channel = input.substring(sigMid + 1, sigEnd).trim();
     message = input.substring(sigEnd + 1).trim();
 
-    return createInfo(timestamp, channel, message, raw);
+    return createInfo(timestamp, channel, message, input);
 }
 
 
