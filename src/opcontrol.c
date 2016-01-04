9705d6f1858b893a89f8274f58b23b2b863d2469
Ernest Wong <ewon521@gmail.com>
2016-01-04 18:10:15 +1300

    Add joystick flywheel presets and adjust buttons

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -6,13 +6,39 @@
 
 #define UNUSED(x) (void)(x)
 
-static void turnOnFlywheel(void*);
+static void turnOnFlywheelShortRange(void*);
+static void turnOnFlywheelLongRange(void*);
 static void turnOffFlywheel(void*);
 static void turnOnConveyor(void*);
 static void turnOffConveyor(void*);
 static void openFlap(void*);
 static void closeFlap(void*);
 static void changeDriveStyle(void*);
+static void increaseFwRpm(void*);
+static void decreaseFwRpm(void*);
+
+typedef enum
+FlywheelPreset
+{
+    FLYWHEEL_SHORTRANGE;
+    FLYWHEEL_LONGRANGE;
+}
+FlywheelPreset;
+
+float fwAbovePresets[2] =
+{
+    1.0f,
+    2.0f
+};
+
+float fwBelowPresets[2] =
+{
+    1.0f,
+    2.0f
+};
+
+bool isFwOn = false;
+FlywheelPreset fwPresetMode = FLYWHEEL_LONGRANGE;
 
 void operatorControl()
 {
@@ -21,9 +47,13 @@ void operatorControl()
     buttonOndown(JOY_SLOT1, JOY_5D, turnOffConveyor, NULL);
     buttonOndown(JOY_SLOT1, JOY_6U, openFlap, NULL);
     buttonOndown(JOY_SLOT1, JOY_6D, closeFlap, NULL);
-    buttonOndown(JOY_SLOT1, JOY_7R, turnOnFlywheel, NULL);
-    buttonOndown(JOY_SLOT1, JOY_7D, turnOffFlywheel, NULL);
     buttonOndown(JOY_SLOT1, JOY_7U, changeDriveStyle, NULL);
+    buttonOndown(JOY_SLOT1, JOY_7R, turnOnFlywheelShortRange, NULL);
+    buttonOndown(JOY_SLOT1, JOY_7D, turnOffFlywheel, NULL);
+    buttonOndown(JOY_SLOT1, JOY_8L, turnOnFlywheelLongRange, NULL);
+    buttonOndown(JOY_SLOT1, JOY_8D, turnOffFlywheel, NULL);
+    buttonOndown(JOY_SLOT1, JOY_8U, increaseFwRpm, NULL);
+    buttonOndown(JOY_SLOT1, JOY_8R, decreaseFwRpm, NULL);
 
     flywheelRun(fwBelow);
     flywheelRun(fwAbove);
@@ -40,10 +70,49 @@ void operatorControl()
 }
 
 static void
-turnOnFlywheel(void * handle)
+turnOnFlywheelLongRange(void * handle)
 {
     UNUSED(handle);
-    flywheelSet(fwAbove, 800);
+    isFwOn = true;
+    fwPresetMode = FLYWHEEL_LONGRANGE;
+    flywheelSet(fwAbove, fwAbovePresets[FLYWHEEL_LONGRANGE]);
+    flywheelSet(fwBelow, fwBelowPresets[FLYWHEEL_LONGRANGE]);
+}
+
+static void
+turnOnFlywheelShortRange(void * handle)
+{
+    UNUSED(handle);
+    isFwOn = true;
+    fwPresetMode = FLYWHEEL_SHORTRANGE;
+    flywheelSet(fwAbove, fwAbovePresets[FLYWHEEL_SHORTRANGE]);
+    flywheelSet(fwBelow, fwBelowPresets[FLYWHEEL_SHORTRANGE]);
+}
+
+static void
+increaseFwRpm(void * handle)
+{
+    UNUSED(handle);
+    fwAbovePresets[fwPresetMode] += 1.0f;
+    fwBelowPresets[fwPresetMode] += 1.0f;
+    if (isFwOn)
+    {
+        flywheelSet(fwAbove, fwAbovePresets[fwPresetMode]);
+        flywheelSet(fwBelow, fwBelowPresets[fwPresetMode]);
+    }
+}
+
+static void
+decreaseFwRpm(void * handle)
+{
+    UNUSED(handle);
+    fwAbovePresets[fwPresetMode] -= 1.0f;
+    fwBelowPresets[fwPresetMode] -= 1.0f;
+    if (isFwOn)
+    {
+        flywheelSet(fwAbove, fwAbovePresets[fwPresetMode]);
+        flywheelSet(fwBelow, fwBelowPresets[fwPresetMode]);
+    }
 }
 
 static void
