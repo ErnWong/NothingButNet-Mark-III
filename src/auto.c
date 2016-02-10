2177137d014b84968dcb1ed52c3f8076cf08e3d7
Ernest Wong <ewon521@gmail.com>
2016-02-10 20:57:20 +1300

    Added (untested) autonomous template (for testing)

--- src/auto.c
+++ src/auto.c
@@ -1,5 +1,44 @@
 #include "main.h"
 
+#include "reckoner.h"
+#include "flywheel.h"
+#include "flap.h"
+#include "diffsteer-control.h"
+
+#define UNUSED(x) (void)(x)
+
+static void updateTask(void*);
+
+static bool isRunning = false;
+
 void autonomous()
 {
+    isRunning = true;
+    TaskHandle task = taskCreate(updateTask, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT);
+
+    diffsteerMove(diffsteer, 1.0f, 1.0f);
+    waitUntilDiffsteerReady(diffsteer, 60000);
+
+    diffsteerRotate(diffsteer, 0.0f);
+    waitUntilDiffsteerReady(diffsteer, 60000);
+
+    diffsteerStop(diffsteer);
+
+    isRunning = false;
+}
+
+static void updateTask(void * arg)
+{
+    UNUSED(arg);
+
+    flywheelRun(fwBelow);
+    flywheelRun(fwAbove);
+    flapRun(fwFlap);
+
+    while (isRunning)
+    {
+        reckonerUpdate(reckoner);
+        diffsteerUpdate(diffsteer);
+        delay(20);
+    }
 }
